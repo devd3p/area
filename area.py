8f578a87667dd6088627f58c6b7ce81861bae289
Vinicius de Souza <110186039+devd3p@users.noreply.github.com>
2023-02-03 12:15:56 -0300

    Add files via upload

---
+++ area.py
@@ -0,0 +1,9 @@
+def area(b,h):
+    print('Calculando a área...')
+    print('Certo! Seu resultado está logo abaixo.')
+    return b * h
+
+b = int(input('Digite o valor da base: '))
+h = int(input('Digite o valor da altura: '))
+
+print(area(b,h))
